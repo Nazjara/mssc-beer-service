10a9765d291d2c315fbfc17e817ade966e087b83
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2019-11-21 10:36:08 +0200

    Implement Brewing service to trigger brewing

---
+++ src/main/java/com/nazjara/service/BrewingService.java
@@ -0,0 +1,41 @@
+package com.nazjara.service;
+
+import com.nazjara.config.JmsConfig;
+import com.nazjara.event.BrewBeerEvent;
+import com.nazjara.mapper.BeerMapper;
+import com.nazjara.model.Beer;
+import com.nazjara.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Scheduled(fixedRate = 5000)
+    public void checkForLowInventory() {
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer invQON = beerInventoryService.getOnHandInventory(beer.getId());
+
+            log.debug("Min on hand is " + beer.getMinOnHand());
+            log.debug("Inventory is " + invQON);
+
+            if (beer.getMinOnHand() >= invQON) {
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+            }
+        });
+    }
+}
